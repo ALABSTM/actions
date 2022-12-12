4ae9deed1d1f68334ee46035737de4efaba7f4d7
Ali Labbene <ali.labbene@st.com>
2022-12-12 13:19:25 +0100

    Color the 'Compile for device ...' message for better readability of the logs

--- compile/entrypoint.sh
+++ compile/entrypoint.sh
@@ -5,6 +5,9 @@
 NOCOLOR='\033[0m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
 
 # Read the arguments passed through
 #  <calling_repo>/.github/workflows/<workflow>/action.yml
@@ -70,7 +73,7 @@ for device in $devices
 do
     # Get the current device's part-number in a variable
     DEFINES='-DUSE_HAL_DRIVER -D'${device}
-    echo "Compiling sources for device ${device} ****************************************************" ;
+    echo -e "\n${CYAN}Compiling sources for device ${device} ****************************************************" ;
     # For each source file, get current source file name in variable "source"
     #  to use it with "echo" and "gcc" commands.
     for source in "${HAL_DIR}/Src"/*.c
